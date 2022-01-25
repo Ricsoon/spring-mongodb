ccff661092d8b01d33d27ecc24a2975a61974982
Ricsoon <painonex2@gmail.com>
2022-01-25 11:54:26 -0300

    Sistema de comentários para um Post

--- src/main/java/com/horizongroup/springmongodb/config/Instantiation.java
+++ src/main/java/com/horizongroup/springmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.horizongroup.springmongodb.domain.Post;
 import com.horizongroup.springmongodb.domain.User;
 import com.horizongroup.springmongodb.dto.AuthorDTO;
+import com.horizongroup.springmongodb.dto.CommentDTO;
 import com.horizongroup.springmongodb.repository.PostRepository;
 import com.horizongroup.springmongodb.repository.UserRepository;
 
@@ -42,6 +43,13 @@ public class Instantiation implements CommandLineRunner {
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Hoje eu acordei feliz.",
 				new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
